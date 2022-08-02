45f18c641f166352c95a0ae774a6f77191e4397b
Romir Patel <romirpatel308@gmail.com>
2022-08-02 10:37:09 -0400

    Project

---
+++ src/home/homePage.js
@@ -0,0 +1,20 @@
+import './home.css';
+
+function homePage() {
+  return (
+    <div >
+      <header className="App-header">
+        <h2>
+         Under Construction
+        </h2>
+        <p> Im working on this site... check back later, maybe it's up maybe it's not </p>
+      </header>
+      
+
+    </div>
+    
+  );
+  
+}
+
+export default homePage;
