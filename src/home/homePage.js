e89192bed20a9335d444635dacaf1b40826429bc
Romir Patel <romirpatel308@gmail.com>
2022-08-12 11:07:50 -0400

    First Commit

--- src/home/homePage.js
+++ src/home/homePage.js
@@ -5,39 +5,10 @@ export default function homePage() {
     <div className='App'>
     
       <header >
-      
-        <h1 className='App-header'>
-         - Romir Patel -
-        </h1>
-        <p> 
-        Web Developer / Cyber Security Enthusiths 
-        </p>
+      <h2> Under Construction</h2>
+      <p>I'm working on this site right now... check back later, maybe it's up maybe it's not</p>
       </header>
-      
-      {/* <header >
-      
-      <nav class="navbar">
-  
-     <div class="logo">Romir Patel</div>
-  
-     <ul class="nav-links">
-     
-       <input type="checkbox" id="checkbox_toggle" />
-       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-    
-       <div class="menu">
-         <li><a href="/">Home</a></li>
-         <li><a href="/">About</a></li>
-         <li><a href="/">Projects</a></li>
 
-         <li><a href="/">Contact</a></li>
-       </div>
-     </ul>
-     
-        </nav>
-     <p className='App-header'> Some text here</p>
-  
-        </header> */}
 
 
     </div>
