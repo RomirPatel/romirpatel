d856aaf5fe954b7efc6ea11cbcbc03b0ed582bf7
Romir Patel <romirpatel308@gmail.com>
2022-08-12 12:18:57 -0400

    Commit

--- src/home/homePage.js
+++ src/home/homePage.js
@@ -1,14 +1,11 @@
 import './home.css';
 
+
 export default function homePage() {
   return (
     <div className='App'>
     
-      <header >
-      <h2> Under Construction</h2>
-      <p>I'm working on this site right now... check back later, maybe it's up maybe it's not</p>
-      </header>
-
+      <h2 className='text-flicker-in-glow'> Test</h2>
 
 
     </div>
